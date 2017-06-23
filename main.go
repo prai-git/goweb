60f6c4a0ee737d2d729d5d3fc19530806e457d4e
Pankaj Rai <prai@gotprint.com>
2017-06-23 17:29:23 +0530

    initial commmit

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
+// routes.go
 package main
 
-func main(){
-  Println("hello");
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/prai-git/upload"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/upload", upload.Upload).Methods("GET")
+	r.HandleFunc("/fileUpload", upload.UploadFile).Methods("POST")
+
+	fmt.Println("Listining port 8080")
+	http.ListenAndServe(":8080", r)
 }
